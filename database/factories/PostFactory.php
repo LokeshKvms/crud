c0e2f47c3507c1dd447bfdd81c40f56b8ec3e04c
LokeshKvms <loki.kvms@gmail.com>
2025-06-02 12:01:10 +0530

    URL params

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            "title"=> fake()->sentence(),
+            "post_text"=> fake()->paragraph(2,true),
+            "category_id"=> Category::inRandomOrder()->first()->id ?? 1,
+        ];
+    }
+}
